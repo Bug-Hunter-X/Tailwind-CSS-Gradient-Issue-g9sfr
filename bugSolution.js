da9f522caba245260d3ce390e2ad4f18b2c5d636
BugHunter-X <askzenai@gmail.com>
2025-02-03 07:36:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// Corrected code
+<div class="bg-gradient-to-r from-blue-500 via-blue-600 to-purple-500 p-4">
+  <p>This is some text.</p>
+</div>
+
+//Or
+
+<div style="background-image: linear-gradient(to right, rgb(59, 130, 246), rgb(156, 163, 255)); padding: 4px;">
+  <p>This is some text.</p>
+</div>
+```
